c16aeec038df9a5a31b53666525861865c53695e
gl_sokolov <gl_sokolov@mail.ru>
2022-10-05 22:11:39 +0300

    Задание 1-4 (дз)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Задание 1
+        for (byte i = 1; i <= 10; i++){
+            System.out.println(+ i);
+        }
+        System.out.println();
+
+        // Задание 2
+        for (byte i = 10; i >= 1; i--){
+            System.out.println(+ i);
+        }
+        System.out.println();
+
+        // Задание 3
+        for (int i = 0; i <= 17; i = i + 2){
+            System.out.println(+ i);
+        }
+        System.out.println();
+
+        // Задание 4
+        for (byte i = 10; i >= -10; i--){
+            System.out.println(+ i);
+        }
     }
 }
