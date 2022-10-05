0e637efde25bdb3f2af4cd744121ae96492750ed
gl_sokolov <gl_sokolov@mail.ru>
2022-10-05 22:29:16 +0300

    Задание 5-7 (дз)

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Задание 1
-        for (byte i = 1; i <= 10; i++){
+        // Задание 5
+        for (int i = 1904; i <= 2096; i = i + 4){
+            System.out.println(i + " Год является високосным");
+        }
+        System.out.println();
+
+        // Задание 6
+        for (int i = 7; i <= 98; i = i + 7){
             System.out.println(+ i);
         }
         System.out.println();
 
-        // Задание 2
-        for (byte i = 10; i >= 1; i--){
+        // Задание 7
+        for (int i = 1; i <= 512; i = i * 2){
             System.out.println(+ i);
         }
-        System.out.println();
 
-        // Задание 3
-        for (int i = 0; i <= 17; i = i + 2){
-            System.out.println(+ i);
-        }
-        System.out.println();
-
-        // Задание 4
-        for (byte i = 10; i >= -10; i--){
-            System.out.println(+ i);
-        }
     }
 }
